ff7954ece056444e5f85332b3c78adc99dee9e30
Gurpreet <gs109690@gmail.com>
2022-09-23 22:49:44 -0500

    wallet connect

--- src/WalletConnect.js
+++ src/WalletConnect.js
@@ -1,7 +1,27 @@
 import { React, useState } from "react";
+import styles from "./Wallet.module.css";
+import { ethers } from "ethers";
 
 const WalletConnect = () => {
-  return <div></div>;
+  const [connButtonText, setConnButtonText] = useState("Connect");
+
+  let provider = new ethers.providers.Web3Provider(window.ethereum);
+  let signer;
+
+  /* const connectButton = () => */ async function connectWalletHandler() {
+    await provider.send("eth_requestAccounts", []);
+    signer = provider.getSigner();
+    setConnButtonText("Connected");
+  }
+
+  return (
+    <div>
+      <h1>React Ethers</h1>
+      <button className={styles.button6} onClick={connectWalletHandler}>
+        {connButtonText}
+      </button>
+    </div>
+  );
 };
 
 export default WalletConnect;
